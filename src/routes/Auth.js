2246c1057fceb27d5f5d79ce4e62f55328f596ef
whalerice <joyfulforest@gmail.com>
2023-09-15 16:56:47 +0900

    Login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,2 +1,48 @@
-const Auth = () => <span>Auth</span>;
+import { useState } from "react";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue width Google</button>
+        <button>Continue width Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
