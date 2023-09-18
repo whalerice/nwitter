2e57739b395872ff6f8d1e49fd88d846676a076a
whalerice <joyfulforest@gmail.com>
2023-09-18 21:14:40 +0900

    firestore db

--- src/fbase.js
+++ src/fbase.js
@@ -16,3 +16,4 @@ firebase.initializeApp(firebaseConfig);
 
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,37 @@
-const Home = () => <span>Home</span>;
+import { dbService } from "fbase";
+import { useState } from "react";
+
+const Home = () => {
+  const [nweet, setNweet] = useState();
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweet").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet || ""}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
