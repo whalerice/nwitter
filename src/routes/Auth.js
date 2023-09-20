2fd1d8634ed9f76e3159a7888e8b76938880e73f
whalerice <joyfulforest@gmail.com>
2023-09-20 18:22:49 +0900

    fixed

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -18,8 +18,7 @@ const Auth = () => {
     } else if (name === "github") {
       provider = new firebaseInstance.auth.GithubAuthProvider();
     }
-    const data = await authService.signInWithPopup(provider);
-    console.log(data);
+    await authService.signInWithPopup(provider);
   };
   return (
     <>
