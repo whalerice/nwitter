72271708c4d7f9fe96e43205846deb9d8e2bc65b
whalerice <joyfulforest@gmail.com>
2023-09-20 15:30:42 +0900

    EDIT PROFILE

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,15 +4,19 @@ import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
-const AppRouter = ({ isLoggedIn, userObj }) => {
+const AppRouter = ({ refreshUser, isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exact path="/" element={<Home userObj={userObj} />} />
-            <Route exact path="/profile" element={<Profile />} />
+            <Route
+              exact
+              path="/profile"
+              element={<Profile userObj={userObj} refreshUser={refreshUser} />}
+            />
           </>
         ) : (
           <>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,55 @@
-import { authService } from "fbase";
+import { authService, dbService } from "fbase";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
   const navigate = useNavigate();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onSignOutClick = () => {
     authService.signOut();
     navigate("/");
   };
+  // const getMyNweets = async () => {
+  //   const data = await dbService
+  //     .collection("nweets")
+  //     .where("creatorId", "==", userObj.uid)
+  //     .orderBy("createdAt")
+  //     .get();
+
+  //   console.log(data.docs.map((doc) => doc.data()));
+  // };
+  // useEffect(() => {
+  //   getMyNweets();
+  // }, []);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={onChange}
+          placeholder="Display Name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onSignOutClick}>Sign Out</button>
     </>
   );
